920188c2dc1016a4e767d9fade359d2926a6e064
Murad Alwan <free1soft@gmail.com>
2018-04-17 12:51:52 +0300

    make Question api

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Question;
 use Illuminate\Http\Request;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -14,18 +15,10 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+
 
     /**
      * Store a newly created resource in storage.
@@ -35,7 +28,15 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        //auth()->user()->questions()->create($request->all());
+
+        Question::create($request->all());
+        return response('Created',200);
+
+
+
+
     }
 
     /**
@@ -46,19 +47,10 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
+        return new QuestionResource($question);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
-    }
+
 
     /**
      * Update the specified resource in storage.
@@ -80,6 +72,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response('Deleted',200);
     }
 }
