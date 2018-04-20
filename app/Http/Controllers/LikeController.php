6decd884ed68cd53ee66eaf73d60e2de9bc612f8
Murad Alwan <free1soft@gmail.com>
2018-04-20 14:40:44 +0300

    makeing JWT INIT.

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,83 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Like;
+use App\Reply;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class LikeController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function like(Reply $reply)
     {
-        //
+
+
+        if (Like::where([
+            'reply_id'=>$reply->id,
+            'user_id'=>1
+        ])->count()>0)
+        {
+            $reply->like()->delete();
+        }
+        else
+        {
+            $reply->like()->create(['user_id'=>1]);
+        }
+        return response('Done',Response::HTTP_ACCEPTED);
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Like $like)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Like $like)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Like $like)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Like  $like
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Like $like)
-    {
-        //
-    }
 }
